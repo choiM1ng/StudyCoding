2d1c5e6e1cebcb2020e41a2796ab0a98279fdbcd
choiM1ng <92712998+choiM1ng@users.noreply.github.com>
2025-04-11 17:50:10 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String s) {
+        int index = s.length()/2;
+        return s.length()%2==0 ? s.substring(index-1, index+1) : s.substring(index, index+1);
+    }
+}
