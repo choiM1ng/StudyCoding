889d845d40330b840229a41bb643e9598fca80f6
choiM1ng <92712998+choiM1ng@users.noreply.github.com>
2025-04-27 02:05:54 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -8,9 +8,7 @@ class Solution {
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        count++;
-                    }
+                    if (number[i] + number[j] + number[k] == 0) count++;
                 }
             }
         }
