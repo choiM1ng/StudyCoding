05083ab227bcf8c81cefcf143773ce6278b3f8d8
choiM1ng <92712998+choiM1ng@users.noreply.github.com>
2026-07-28 00:15:23 +0900

    [level 1] Title: 추억 점수, Time: 1.15 ms, Memory: 93.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for(String s : photo[i]) {
+                if(map.containsKey(s)) {
+                    sum += map.get(s);
+                }
+            }
+            answer[i] = sum;
+        }
+
+        return answer;
+    }
+}
